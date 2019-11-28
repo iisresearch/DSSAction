46f3f1b9eed3d3a8932a01c491227d92177bca87
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-11-28 15:24:45 +0100

    Update concerning session

--- src/main/java/ch/iisresear/dssa/service/ChatService.java
+++ src/main/java/ch/iisresear/dssa/service/ChatService.java
@@ -10,11 +10,10 @@ import ch.iisresear.dssa.data.domain.TrainingData;
 import ch.iisresear.dssa.data.repository.TrainingDataRepository;
 import ch.iisresear.dssa.model.Story;
 import gate.util.GateException;
+import org.apache.commons.collections4.map.PassiveExpiringMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 @Service
@@ -22,14 +21,14 @@ public class ChatService {
     private TrainingDataRepository trainingDataRepository;
     private GateService gateService;
     private DSSAProperties dssaProperties;
-    private Map<String,Story> stories;
+    private PassiveExpiringMap<String, Story> stories;
 
     @Autowired
     public ChatService(TrainingDataRepository trainingDataRepository, GateService gateService, DSSAProperties dssaProperties) {
         this.trainingDataRepository = trainingDataRepository;
         this.gateService = gateService;
         this.dssaProperties = dssaProperties;
-        stories = new HashMap<String,Story>();
+        stories = new PassiveExpiringMap<>(1200000); // Dialogflow has a session of 20 minutes
     }
 
     public String message(String utterance, String userId){
